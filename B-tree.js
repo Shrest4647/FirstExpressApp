25c7fa522a14625c3b0e55466d9115b3b04c35e2
Sunil Shrestha <35072109+Shrest4647@users.noreply.github.com>
2019-07-17 22:10:38 +0545

    added insertion functions to B tree
    Co-Authored-By: Sunil Shrestha <shrest4647@users.noreply.github.com>

--- B-tree.js
+++ B-tree.js
@@ -1,30 +1,29 @@
-class BTreeNode{
-    constructor(t,leaf){
+function  BTreeNode(t,leaf){
         this.t= t;
         this.leaf = leaf;
-        this.keys =[];
-        this.Child = [];
+        this.keys =new Array(2*this.t-1);
+        this.C = new Array(2*this.t);
         this.n = 0;
 
-    }
-    traverse(){
-        for(var i=0;i<this.n;i++)
+    this.traverse= function(){
+        var i;
+        for(i=0;i<this.n;i++)
         {
             if(this.leaf==false)
             {
-                this.Child[i].traverse();
+                this.C[i].traverse();
 
             }
             console.log(this.keys[i]);
         }
         if(this.leaf==false)
         {
-            this.Child[i].traverse();
+            this.C[i].traverse();
         }
 
     }
 
-    search(int k){
+    this.search = function(k){
         var i=0;
         while(i<this.n && k>this.keys[i]){
             i++;
@@ -35,29 +34,140 @@ class BTreeNode{
         if(this.leaf ==true){
             return null;
         }
-        return this.Child[i].search(k);
+        return this.C[i].search(k);
     }
 
-}
+    this.insertNonFull= function(k){
+        var i = this.n-1;
+        if(this.leaf ==true){
+            while(i>=0 && this.keys[i]>k){
+                this.keys[i+1] =this.keys[i];
+                i--;
+            }
 
-class BTree{
-    constructor(t){
+            this.keys[i+1]=k;
+            this.n++;
+
+
+        }
+
+        else{
+            while(i>=0 && this.keys[i]>k){
+                i--;
+            }
+            if(this.C[i+1].n===2*this.t-1){
+                splitChild(i+1,this.C[i+1]);
+                if(this.keys[i+1]<k){
+                    i++;
+                }
+            }
+            this.C[i+1].insertNonFull(k);
+        }
+
+    }
+
+    this.splitChild = function(i,y){
+        var z = new BTreeNode(y.t,y.leaf);
+        z.n = this.t-1;
+
+        for(var j =0;j<this.t-1;j++){
+            z.keys[j]= y.keys[j+t];
+        }
+        if(y.leaf == false){
+            for(var j=0;j<t;j++){
+                z.C[j]= y.C[j+this.t];
+            }
+        }
+
+        y.n= this.t-1;
+
+        for(var j=this.n;j>=i+1;j--){
+            this.C[j+1] = this.C[j];
+        }
+
+        this.C[i+1]=z;
+
+        for(var j=this.n-1;j>=i;j--){
+            this.keys[j+1]= this.keys[j];
+        }
+
+        this.keys[i] = y.keys[t-1];
+
+        this.n++;
+
+
+
+    }
+
+
+};
+
+function BTree(t){
         this.root = null;
         this.t = t;
-    }
-    search(data,){
+
+    this.search = function(data){
         if(this.root==null){
             return null;
         }
         else{
-            return this.root.traverse();
+            return this.root.search(data);
         }
 
 
     }
-    traverse(){
+    this.traverse= function(){
         if(this.root!==null){
             this.root.traverse();
         }
     }
-}
+
+    this.insert= function(k){
+        if(this.root ==null){
+            this.root = new BTreeNode(this.t,true);
+            this.root.keys[0]= k;
+            this.root.n=1;
+        }
+        else{
+            if(this.root.n==2*this.t-1){
+                var s = new BTreeNode(this.t,false);
+                s.C[0]= this.root;
+                s.splitChild(0,this.root);
+
+                var i =0;
+                if(s.keys[0]<k){
+                    i++;
+                }
+                s.C[i].insertNonFull(k);
+                this.root = s;
+            }
+            else{
+                console.log(this.root);
+                this.root.insertNonFull(k);
+            }
+        }
+
+    }
+
+    
+};
+
+
+t= new BTree(3); // A B-Tree with minium degree 3 
+t.insert(10); 
+t.insert(20); 
+t.insert(5); 
+t.insert(6); 
+t.insert(12); 
+t.insert(30); 
+t.insert(7); 
+t.insert(17); 
+
+console.log("Traversal of the constucted tree is "); 
+t.traverse(); 
+
+var k = 6; 
+console.log((t.search(k) != null)? "\nPresent" : "\nNot Present"); 
+
+k = 15; 
+console.log((t.search(k) != null)? "\nPresent" : "\nNot Present"); 
