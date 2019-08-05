1216405485f8e767d9e3b0a0e425264bb3f1b141
Sunil Shrestha <35072109+Shrest4647@users.noreply.github.com>
2019-08-05 18:29:20 +0545

    ENviroment for deploying
    Deploying issues fixed

--- App.js
+++ App.js
@@ -35,39 +35,4 @@ app.get("*",function(req,res){
 
 
 
-// app.get("/", function (req, res) {
-//     res.send("Hi there Welcome to the assignment");
-// });
-
-// app.get("/speak/:animal", function (req, res) {
-//     var A = req.params.animal.toLowerCase();
-//     if(A == "pig")
-//     {
-//         res.send("The pig says 'Oink'");
-//     }
-//     else if(A=="cow"){
-//         res.send("The cow says 'Moo'");
-//     }
-//     else if(A== "dog"){
-//         res.send("The dog says 'Woof Woof'");
-//     }
-//     else{
-//         res.send("Sorry")
-//     }
-// });
-// app.get("/repeat/:stri/:no",function(req,res){
-//     var parm = req.params;
-//     var str = "";
-//     for(let i =0;i<parm.no;i++)
-//     {
-//         str+=parm.stri+" ";
-//     }
-//     res.send(str);
-// });
-
-// app.get("*",function(req,res){
-//     res.send("Sorry page not found");
-// })
-app.listen(3000, function () {
-    console.log("Server Started please proceed to browser");
-})
+app.listen(process.env.PORT,process.env.IP);
